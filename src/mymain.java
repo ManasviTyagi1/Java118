3e748645ecb0ff85d431a5f493f9e553b53edb26
MANASVI <tyagimanasvityagi@gmail.com>
2025-08-27 13:13:35 +0530

    27 August 2025

--- src/mymain.java
+++ src/mymain.java
@@ -3,7 +3,7 @@ public class mymain {
     public static void main(String[] args) {
 //        STUDENT s1=new STUDENT(18 , "VIRAT KOHLI");
 //        STUDENT s2=new STUDENT(s1);
-//
+//h
 //        s1.display();
 //        s2.display();
         Myprint my = new Myprint();
